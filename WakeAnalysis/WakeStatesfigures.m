672c406ea2257770a97304162506b0a31ba98423
NewNitzan <nitzan.albeck@gmail.com>
2025-06-30 16:17:01 +0300

    example trace for PV157,h5.
    in wakefigures

--- WakeAnalysis/WakeStatesfigures.m
+++ WakeAnalysis/WakeStatesfigures.m
@@ -114,13 +114,101 @@ fileName=[analysisFolderWake filesep 'ExampleTraces-PV126H54'];
 print(fileName,'-dpdf','-r300')
 
 
-%% Check the video annotations:
-videoPath = WA.recTable.VideoFiles{WA.currentPRec};
-matPath = WA.files.ArenaLocation;
-xColumnName = 'x__nose___cam_x__';
-yColumnName = 'x__nose___cam_y__';
+%% example plot - old - only LFP and behavior - PV157,H5
+% plot example trial - nose y-time, LFPs
+% set the start of example timefrom rec + zeroFrame
+WA.setCurrentRecording('Animal=PV157,recNames=Hunter5')
+ArenaCSVs = WA.getArenaCSVs(1);
+locTable = WA.getHeadPosition;
 
-overlayPointsOnVideo(videoPath, matPath, xColumnName, yColumnName)
+
+zeroTime = 82812.4; 
+inds = find(ArenaCSVs.oeCamTrigs<zeroTime); 
+zeroFrame = inds(end)+1;
+
+% get the recdata 
+fullwin = 14000;
+[recData_M,recData_T] = WA.currentDataObj.getData(WA.recTable.defaulLFPCh(WA.currentPRec),zeroTime,fullwin);
+recData_M = squeeze(recData_M);
+
+% get the behavioral data:
+frameWin = round(fullwin/(1000/ArenaCSVs.videoFPS));
+nose_y = locTable.x__nose___y__(zeroFrame:zeroFrame+frameWin-1); 
+nose_x = locTable.x__nose___y__(zeroFrame:zeroFrame+frameWin-1);
+nose_t = linspace(0,fullwin/1000,frameWin);
+
+% plot
+figure;
+set(gcf,'Position',[100 20 800 600])
+ax1 = subplot(3,3,[1:3]);
+%plot the nose location
+plot(nose_t,nose_y);
+ylabel('Nose from screen [cm]');
+ylim([-20 90])
+% plot the LFPs
+ax2 = subplot(3,3,[4:6]);
+plot(recData_T/1000,recData_M, 'Color','black');
+ylabel('uV');
+xlabel('Time[s]');
+
+linkaxes([ax1,ax2],'x');
+
+% add the timings of the strik and end and start trial. 
+startTime = (ArenaCSVs.startFrameSh(1)-zeroFrame)/(1000/30);
+strikeFrame = (ArenaCSVs.strikeFrameSh(1)+2);
+strikeTime = (ArenaCSVs.oeCamTrigs(strikeFrame)-zeroTime)/1000;
+endtime = (ArenaCSVs.endTrigSh(1)-zeroTime)/1000;
+xline(ax1,startTime,'b--' ,'LineWidth', 2, 'Label', 'Bug Apperance', 'LabelVerticalAlignment', 'top', 'LabelHorizontalAlignment', 'right');
+xline(ax2,startTime,'b--','LineWidth', 2);
+xline(ax1,strikeTime,'r--' ,'LineWidth', 2, 'Label', 'Strike', 'LabelVerticalAlignment', 'top', 'LabelHorizontalAlignment', 'right');
+xline(ax2,strikeTime,'r--','LineWidth', 2);
+xline(ax1,endtime,'g--' ,'LineWidth', 2, 'Label', 'Bug Disapearance', 'LabelVerticalAlignment', 'top', 'LabelHorizontalAlignment', 'right');
+xline(ax2,endtime,'g--','LineWidth', 2);
+hold on
+fs = WA.currentDataObj.samplingFrequency(WA.recTable.defaulLFPCh(WA.currentPRec));
+cutoff1 = 2.5*fs:3.5*fs;
+yLimits = ylim;
+patch([cutoff1(1)/fs cutoff1(end)/fs  cutoff1(end)/fs cutoff1(1)/fs], ...
+      [yLimits(1) yLimits(1) yLimits(2) yLimits(2)], ...
+      'b', 'FaceAlpha', 0.3, 'EdgeColor', 'none'); % Highlight with a semi-transparent red
+cutoff2 = 5*fs:6*fs;
+patch([cutoff2(1)/fs cutoff2(end)/fs  cutoff2(end)/fs cutoff2(1)/fs], ...
+      [yLimits(1) yLimits(1) yLimits(2) yLimits(2)], ...
+      'r', 'FaceAlpha', 0.3, 'EdgeColor', 'none'); 
+
+cutoff3 = 12*fs:13*fs;
+patch([cutoff3(1)/fs cutoff3(end)/fs  cutoff3(end)/fs cutoff3(1)/fs], ...
+      [yLimits(1) yLimits(1) yLimits(2) yLimits(2)], ...
+      'g', 'FaceAlpha', 0.3, 'EdgeColor', 'none'); % Highlight with a semi-transparent red
+hold off
+
+% add close up graphs:
+ax3 = subplot(3,3,7);
+plot(recData_T(cutoff1)/1000,recData_M(cutoff1), 'Color','black');
+xlabel('Time[s]');
+ylabel('uV');
+
+ax4 = subplot(3,3,8);
+plot(recData_T(cutoff2)/1000,recData_M(cutoff2), 'Color','black');
+xlabel('Time[s]');
+
+ax5 = subplot(3,3,9);
+plot(recData_T(cutoff3)/1000,recData_M(cutoff3), 'Color','black');
+
+xlabel('Time[s]');
+linkaxes([ax3,ax4,ax5],'y');
+title('PV157,H5 - Example trial')
+
+%saveFigure:
+set(gcf,'PaperPosition',[.25 3 8 6])
+
+saveas(gcf,strcat(WA.currentPlotFolder, '/TrialNoiseHL.pdf'));
+fileName=[analysisFolderWake filesep 'ExampleTrailPV157,H5'];
+print(fileName,'-dpdf',['-r' num2str(SA.figResJPG)]);
+
+
+
+
 
 %% get beta to gamma ratio:
 WA.setCurrentRecording('Animal=PV157,recNames=Hunter5');
@@ -145,9 +233,6 @@ BG = WA.getMultiBandSpectralAnalysis('band1Low', 60,'band1High',80,'band2Low', .
 p = find(~isnan(BG.band2to1Ratio));
 BG.band2to1Ratio = BG.band2to1Ratio(p);
 BG.t_ms = BG.t_ms(p);
-
-ArenaCSV = WA.getArenaCSVs;
-
 
 %% Create the plot
 figure;
@@ -174,3 +259,15 @@ xlabel('Time [s]');
 legend('B/G', 'Trial Time', 'Location', 'best');
 
 hold off;
+
+
+
+
+
+%% Check the video annotations:
+videoPath = WA.recTable.VideoFiles{WA.currentPRec};
+matPath = WA.files.ArenaLocation;
+xColumnName = 'x__nose___cam_x__';
+yColumnName = 'x__nose___cam_y__';
+
+overlayPointsOnVideo(videoPath, matPath, xColumnName, yColumnName)
