29961db4394057ca2a0d0c5437a65b5b8b916ff1
NewNitzan <nitzan.albeck@gmail.com>
2026-02-19 15:11:03 +0200

    adding a sort of trying to get the beta/

---
+++ Sleep_state_Manipulation_Light/getBetaStim.m
@@ -0,0 +1,71 @@
+% get beta stim
+%% getStimSham
+% this functio is getting the data rellevant for the stimulation and the
+% "sham" stimulation. It should save the data in the table. 
+
+function data = getBetaStim(SA, overwrite)
+    % SA is an instance of sleep analysis class,with a record currently
+    % selected
+    if nargin <2 
+        overwrite = 0;
+    end
+    %check if analysis was already done
+    SA.files.betaStim=[SA.currentAnalysisFolder filesep 'betaStim.mat'];
+    if exist(SA.files.betaStim,'file') & ~overwrite
+        if nargout==1
+            data=load(SA.files.betaStim);
+        else
+            disp('stim sham analysis already exists for this recording');
+        end
+        return;
+    end
+
+    DB=SA.getDelta2BetaRatio;
+    % AC=SA.getDelta2BetaAC;
+    
+    stimData = SA.getStimTriggers;
+    firstTrig = stimData(1:8:end-2);
+    endStim = stimData(8:8:end)+200;
+    stimDuration=(endStim(1)-firstTrig(1));
+
+     
+    pre=50000; %ms
+    post=100000; %ms
+%     clear StimDB; %change to zeros
+    win = (pre+post)/1000;
+    
+    stimBeta = zeros(numel(firstTrig),win);
+    for i=1:numel(firstTrig)
+        pTmp=find(DB.t_ms>(firstTrig(i)-pre) & DB.t_ms<=(firstTrig(i)+post));
+        %StimDB(i,:)=1./DB.bufferedDelta2BetaRatio(pTmp);
+        if length(pTmp) ~=win
+            pTmp = ones([1,win]);
+        end
+        stimBeta(i,:)=DB.bufferedBetaRatio(pTmp);
+    end
+
+    ts=(DB.t_ms(pTmp)-DB.t_ms(pTmp(1)))/1000;% creating Time array
+    % meadStimInterval=mean(diff(firstTrig)); %array of times between stims
+    % firstTrigSham=(AC.tStartSleep:meadStimInterval:(firstTrig(1)-post))+10000;
+    % endStimSham=firstTrigSham+max(endStim-firstTrig);
+    
+% %     clear StimDBSham;
+%     for i=1:numel(firstTrigSham)
+%         pTmp=find(DB.t_ms>(firstTrigSham(i)-pre) & DB.t_ms<=(firstTrigSham(i)+post));
+%         %StimDBSham(i,:)=1./DB.bufferedDelta2BetaRatio(pTmp);
+%         if length(pTmp) ~=150
+%             continue
+%         end
+%         StimDBSham(i,:)=DB.bufferedDelta2BetaRatio(pTmp);
+%     end
+
+   % save the data
+save(SA.files.betaStim,'ts','stimBeta','stimDuration','pre','post')
+% data.StimDBSham = StimDBSham;
+data.ts = ts;
+data.stimBeta = stimBeta;
+data.stimDuration = stimDuration;
+data.pre = pre;
+data.post = post;
+
+end
